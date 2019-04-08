509419bff94a7b6c49c35d21a852d28dbadb93c1
kevan <kevan.tan@live.com>
2019-04-08 09:45:29 +0800

    changed bus interval to 15s

--- js/game.js
+++ js/game.js
@@ -116,7 +116,7 @@ gameScene.create = function() {
   // event that spawns buses
 
   this.busEvent = this.time.addEvent({
-    delay: 10000,
+    delay: 15000,
     callback: gameScene.generateBuses,
     callbackScope:this,
     loop:true
